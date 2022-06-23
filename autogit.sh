f5e5d6d6f2f3052f32f1caea90e6285641b2c9f8
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-06-23 12:04:07 -0300

    Link da Apresentacao add no README | Prox. Passo: Adicionar várias séries ao gráfico de dispersão R

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add -A
 git commit -m "Link da Apresentacao add no README | Prox. Passo: Adicionar várias séries ao gráfico de dispersão R"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/Apresent-Estatist.git
-git push -u origin main
-git status
+git push
+exit
